d8d0c16781347fa12388ec4bb87ee45220e643f2
williamarchbold <williamsarchbold@gmail.com>
2019-02-02 18:28:14 -0700

    no message

---
+++ Project 2 - OOP Character Battle/Project 2 - OOP Character Battle/Character.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Project_2___OOP_Character_Battle {
+
+    public class Character {
+
+        private int moveSpeed;
+        private int damagePerAttack;
+        private int health;
+        private int position;
+        private int priority;
+        private int attackRange;
+
+        public void TakeDamage(int amount);
+
+        public string GetMovementDescription() {
+            return ToString("Movespeed: {0} Attack Range: {1}  Damage: {2}", moveSpeed, attackRange, damagePerAttack);
+        }
+
+        abstract string GetSpecialDescription() {
+            return ToString("Special attack description: ");
+        }
+
+        public string Attack() {
+            return "Attack was: ";
+        }
+
+        abstract string Special(Character target) {
+            return "Here's what happened...";
+        }
+
+
+
+
+    }
+}
---
+++ Project 2 - OOP Character Battle/Project 2 - OOP Character Battle/Mage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2___OOP_Character_Battle {
+
+    class Mage : Character {
+
+        Mage()
+        {
+            
+        }
+    }
+}
