e37d65a797a8998a3c4d53f520aa6de38d724c6d
williamarchbold <williamsarchbold@gmail.com>
2019-02-12 21:45:10 -0700

    Able to find and concatenate file name, but can't keep it when going back up recursion

---
+++ Lab 2/Lab 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class RecursiveFileFinder
+    {
+        static void Main(string[] args) {
+
+            //string[] directories = Directory.GetDirectories(AppDomain.CurrentDomain.BaseDirectory + @"FolderTree");
+
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"FolderTree";
+
+            //using (StreamReader sr = new StreamReader(path)) {
+            string result = findFile(path);
+            Console.WriteLine("File location: {0}", result);
+            //}
+            Console.WriteLine("Type any key to continue...");
+            Console.ReadKey();
+
+            
+
+        }
+
+        static string findFile(string path) {
+            foreach (DirectoryInfo directory in new DirectoryInfo(path).GetDirectories())
+            {
+                findFile(path + "\\" + directory);
+
+                foreach (FileInfo file in new DirectoryInfo(directory.FullName).GetFiles())
+                {
+                    path+= "\\" + file;
+                    return path;
+                    
+                }
+            }
+            return path;
+        }
+    }
+}
